6c35e394e551c2472733b3210f7093fdbdde9011
Patrick O'Brien <obrienp@outlook.com>
2017-12-02 22:10:55 -0600

    added simple data table

--- src/main/resources/com/pmobrien/vultus/liftoff/webapp/script.js
+++ src/main/resources/com/pmobrien/vultus/liftoff/webapp/script.js
@@ -1,5 +1,27 @@
 function get() {
-  $.get("/api/scores", function(data) {
-    console.log(data);
+  $.getJSON('/api/scores', null, function(json) {
+    table.fnClearTable();
+
+    for(var i = 0; i < json.length; ++i) {
+      table.oApi._fnAddData(table.fnSettings(), json[i]);
+    }
+    
+    table.fnDraw();
+    table.fnSort([[4, 'desc']]);
   });
 }
+
+$(document).ready(function() {
+  window.table = $('#scores_table').dataTable({
+    paging: false,
+    searching: false,
+    info: false,
+    columns: [
+      { data: 'username' },
+      { data: 'snatch' },
+      { data: 'cleanAndJerk' },
+      { data: 'metcon' },
+      { data: 'score' }
+    ]
+  });
+});
