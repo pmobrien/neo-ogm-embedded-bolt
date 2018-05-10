36881610231b0811e2428d986fca0912dbf54972
Patrick O'Brien <obrienp@outlook.com>
2018-05-10 14:54:37 -0500

    added first query; removed extra resources

--- src/main/java/com/pmobrien/Main.java
+++ src/main/java/com/pmobrien/Main.java
@@ -7,23 +7,40 @@ import com.pmobrien.neo.pojo.StorageResource;
 public class Main {
 
   public static void main(String[] args) {
+    StorageResource base = NeoEntity.create(StorageResource.class)
+        .setName("storage:/base");
+
+    StorageResource top = NeoEntity.create(StorageResource.class)
+        .setName("top")
+        .setParent(base);
+
+    StorageResource folder = NeoEntity.create(StorageResource.class)
+        .setName("folder")
+        .setParent(top);
+
+    StorageResource file = NeoEntity.create(StorageResource.class)
+        .setName("file.txt")
+        .setParent(folder);
+    
     Sessions.sessionOperation(session -> {
-      StorageResource base = NeoEntity.create(StorageResource.class)
-          .setName("storage:/base");
-      
-      StorageResource top = NeoEntity.create(StorageResource.class)
-          .setName("top")
-          .setParent(base);
-      
-      StorageResource folder = NeoEntity.create(StorageResource.class)
-          .setName("folder")
-          .setParent(top);
-      
-      StorageResource file = NeoEntity.create(StorageResource.class)
-          .setName("file.txt")
-          .setParent(folder);
-      
       session.save(file);
+    });
+    
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println("Begin queries:");
+    
+    Sessions.sessionOperation(session -> {
+      StorageResource resource = session.load(StorageResource.class, file.getUuid());
+      
+      System.out.println("Only loads one level of relationships so only get one parent here (as expected).");
+      System.out.print(String.format("(%s)", resource.getName()));
+      
+      while(resource.getParent() != null) {
+        System.out.print(String.format("<-[PARENT_OF]-(%s)", resource.getParent().getName()));
+        resource = resource.getParent();
+      }
     });
     
     while(true) {}
