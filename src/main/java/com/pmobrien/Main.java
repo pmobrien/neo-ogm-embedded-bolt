801094e1f3842e1dad43cf0b31be541c03b98c60
Patrick O'Brien <obrienp@outlook.com>
2018-05-14 08:33:58 -0500

    removed methods not used on this branch

--- src/main/java/com/pmobrien/Main.java
+++ src/main/java/com/pmobrien/Main.java
@@ -45,36 +45,4 @@ public class Main {
     
     while(true) {}
   }
-  
-  // This is the logic that I want to get rid of. Rather than having to do this every time, I just want to get a single
-  // reference back to the bottom-most StorageResource node, with parents wired up properly.
-  private static StorageResource findResource(List<StorageResource> resources) {
-    // Not sure what order nodes are returned in the list, but all of the parent/children relationships are wired up
-    // properly for each row so just pick the first one and find its root.
-    StorageResource resource = resources.get(0);
-    while(resource.getParent() != null) {
-      resource = resource.getParent();
-    }
-    
-    // Now that we are at the root, traverse down to the lowest child. We aren't querying for any siblings so
-    // getChildren().get(0) will always work.
-    while(resource.getChildren() != null && !resource.getChildren().isEmpty()) {
-      resource = resource.getChildren().get(0);
-    }
-    
-    return resource;
-  }
-  
-  private static void printResource(StorageResource resource) {
-    StorageResource current = resource;
-    
-    System.out.println();
-    System.out.println("Path:");
-    System.out.print(String.format("(%s)", current.getName()));
-      
-    while(current.getParent() != null) {
-      System.out.print(String.format("<-[PARENT_OF]-(%s)", current.getParent().getName()));
-      current = current.getParent();
-    }
-  }
 }
