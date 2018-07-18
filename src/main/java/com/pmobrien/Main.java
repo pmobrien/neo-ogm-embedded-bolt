5c5d457c8049656238a57b731e432a5ea34ad1f9
Patrick O'Brien <obrienp@outlook.com>
2018-07-18 16:02:54 -0500

    set up thread pool

--- src/main/java/com/pmobrien/Main.java
+++ src/main/java/com/pmobrien/Main.java
@@ -3,8 +3,12 @@ package com.pmobrien;
 import com.pmobrien.neo.Sessions;
 import com.pmobrien.neo.pojo.NeoEntity;
 import com.pmobrien.neo.pojo.StorageResource;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
+  
+  private static final ExecutorService POOL = Executors.newFixedThreadPool(5);
 
   public static void main(String[] args) {
     StorageResource base = NeoEntity.create(StorageResource.class)
@@ -13,6 +17,12 @@ public class Main {
     Sessions.sessionOperation(session -> {
       session.save(base);
     });
+    
+    for(int i = 0; i < 100; ++i) {
+      POOL.submit(() -> Sessions.sessionOperation(session -> {
+        session.save(NeoEntity.create(StorageResource.class).setName("filename"));
+      }));
+    }
     
     while(true) {}
   }
